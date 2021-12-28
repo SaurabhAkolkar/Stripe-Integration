c0ac518ed08141204e6a3f4ae1bed4a744eea84b
SaurabhAkolkar <37813446+SaurabhAkolkar@users.noreply.github.com>
2021-12-28 19:24:36 +0530

    first commit

---
+++ app/Jobs/CustomerCreatedJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Models\WebhookCall;
+use App\Models\Plan;
+
+class CustomerCreatedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /** @var \Spatie\WebhookClient\Models\WebhookCall */
+    public $webhookCall;
+
+    public function __construct(WebhookCall $webhookCall)
+    {
+        $this->webhookCall = $webhookCall;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Plan::create([
+            'name'=>'aaaaaaaaaaCustomer Created Plan',
+            'slug'=>'caaaaaaaaaaustomer-plan-success',
+            'stripe_plan'=>'aaaaaaaasflkjasdflkj',
+            'cost'=>67,
+            'description'=>'Daaaaiscription'
+        ]);
+    }
+}
